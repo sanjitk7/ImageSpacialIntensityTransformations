ce6bda3f63ffed4e43cb38a315aaf66647aa0515
Sanjit Kumar <sanjitk2018@gmail.com>
2020-09-28 23:00:26 +0530

    point spacial operations with PIL

--- image_processing_da.py
+++ image_processing_da.py
@@ -1,5 +1,42 @@
-import cv2
-img = cv2.imread("pic.png")
-cv2.imshow("image",img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+from PIL import Image
+from math import log10
+import numpy as np
+
+im = Image.open("pic.png").convert("LA")
+pixelMap = im.load()
+# im.show()
+# print(pixelMap[200,200])
+
+
+# Listing All The pixel Values of the Image
+# for i in range (im.size[0]):
+#     for j in range(im.size[1]):
+#         print("f(" + str(i) + "," + str(j)+") = " + str(pixelMap[i,j][0]))
+
+
+# SPACIAL IMAGE OPERATIONS - SINGLE PIXEL OPERATIONS
+
+# # 1. IMG NEGETIVE
+
+# # for i in range (im.size[0]):
+# #     for j in range(im.size[1]):
+# #         pixelMap[i,j] = (int(256-1-pixelMap[i,j][0]),pixelMap[i,j][1])
+# #         # print(pixelMap[i,j])
+# # im.show()
+        
+# # # 2. LOG TRANSFORMS
+
+# # for i in range (im.size[0]):
+# #     for j in range(im.size[1]):
+# #         pixelMap[i,j] = (int(40*np.log(1 + pixelMap[i,j][0])),pixelMap[i,j][1])
+# #         # print(pixelMap[i,j])
+# im.show()
+
+# 3. POWER-LAW TRANSFORMATIONS
+
+gamma = 4
+c = 1
+for i in range (im.size[0]):
+    for j in range(im.size[1]):
+        pixelMap[i,j] = (int(c*255*(pixelMap[i,j][0]/255)**(1/gamma)),pixelMap[i,j][1])
+im.show()
